75fae435b4f3607c2b4f3265486057a54bea9d2a
shashperera <it17097598@my.sliit.lk>
2019-04-10 11:58:56 +0530

    tour details  page

---
+++ src/components/tourDetails.js
@@ -0,0 +1,20 @@
+import React, {Component} from 'react';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import axios from 'axios';
+import "bootstrap/dist/css/bootstrap.min.css";
+
+export default class tourDetails extends Component {
+
+    render() {
+        return (
+            <div class="card" style="width: 18rem;">
+            
+                <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <a href="#" class="btn btn-primary">Go somewhere</a>
+                </div>
+            </div>
+        )
+    }
+}
